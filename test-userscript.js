d11cab146bb37da5905d2de820a11fc1b9f1b315
Tim Churchward <git@timchurchward.com.au>
2024-11-07 20:55:23 +1100

    Initial commit

---
+++ test-userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Test Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Test loading script from GitHub
+// @author       You
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/tinmancactus/test/main/hello.js
+// @require      https://raw.githubusercontent.com/tinmancactus/test/main/foo.js
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  // Your code here (optional)
+  console.log(foo);
+})();
